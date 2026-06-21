4a39095cfeb07a0045d0f2698708579d81a4ec37
sudhanshunitinatalkar <atalkarsudhanshu@proton.me>
2026-06-21 19:43:05 +0530

    update

--- flake.nix
+++ flake.nix
@@ -4,9 +4,18 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     cosmic = {
       url = "github:0xnryn/cosmic";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    opinions = {
+      url = "github:0xnryn/opinions";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    sudha = {
+      url = "github:0xnryn/sudha";
+      inputs.nixpkgs.follows = "nixpkgs";
     };
     home-manager = {
-      url = "github:nix-community/home-manager/release-26.05";
+      url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     flake-parts = {
@@ -24,32 +33,20 @@
       url = "github:nix-community/NixOS-WSL/main";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    comin = {
-      url = "github:nlewo/comin";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
     sops-nix = {
       url = "github:Mic92/sops-nix";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-    agenix = {
-      url = "github:ryantm/agenix";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     lanzaboote = {
       url = "github:nix-community/lanzaboote";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    wrapper-modules = {
-      url = "github:BirdeeHub/nix-wrapper-modules";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
   };
   outputs = inputs:
   inputs.flake-parts.lib.mkFlake { inherit inputs; } {
-    systems = [ "x86_64-linux" "aarch64-linux" ]; 
     imports = [ 
       (inputs.import-tree ./modules) 
+      inputs.cosmic.flakeModules.default
     ];
   };
 }
